84a5c73a242a0ae5c3a9369a2046d63125c8264d
kambleps <125243027+kambleps@users.noreply.github.com>
2023-06-23 20:15:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let[username,setUsername]=useState("")
+  let[pass,setPass]=useState("")
+  let[err,setErr]=useState("")
+  function change(e){
+    if(name==="" || pass===""){
+      setErr("Both the username and password required")
+    }
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+    <input type="text" value={name} onChange={(e)=>setName(e.target.value)}> </input>
+     <input type="pass" value={pass} onChange={(e)=>setPass(e.target.value)}> </input>
+     { err && <p id="errorMessage">{err}</p>
+       <button onClick={change}></button>
     </div>
   )
 }
