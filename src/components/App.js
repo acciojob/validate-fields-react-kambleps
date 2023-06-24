fd094f0058953a04e17cfd5a934649e6a44d8061
kambleps <125243027+kambleps@users.noreply.github.com>
2023-06-24 12:37:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ const App = () => {
         {/* Do not remove the main div */}
     <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)} />
      <input type="password" value={pass} onChange={(e)=>setPass(e.target.value)} />
-     { err && (<p id="errorMessage">{err}</p> )}
+      <p id="errorMessage">{err}</p>
        <button onClick={change}>Login</button>
     </div>
   )
